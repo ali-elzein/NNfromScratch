06895b2775a94c63a7f0777829cd7b6684731a56
Ali El-Zein <shaclata@gmail.com>
2024-04-24 00:59:34 -0500

    Added a layer of neurons (vid 2)

--- src/p1.py
+++ src/p1.py
@@ -2,9 +2,25 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1.2, 5.1, 2.1] # output from 3 neurons in prev layer
-weights = [3.1, 2.1, 8.7] # weight for each input
-bias = 3
+def neuron_output(weights, inputs, bias):
+    output = 0
+    for i in range(len(inputs)):
+        output += inputs[i] * weights[i]
+    return output + bias
+    
 
-output = inputs[0] * weights[0] + inputs[1] * weights[1] + inputs[2] * weights[2] + bias # neuron output
-print(output)
+inputs = [1, 2, 3, 2.5] # output from 3 neurons in prev layer
+weights1 = [0.2, 0.8, -0.5, 1.0] # weight for each input into neuron1
+weights2 = [0.5, -0.91, 0.26, -0.5]
+weights3 = [-0.26, -0.27, 0.17, 0.87]
+
+bias1 = 2 # bias used to calc output from neuron1
+bias2 = 3
+bias3 = 0.5
+
+# calculated output for the 3 neurons in the layer
+layer_output = [neuron_output(inputs, weights1, bias1),
+                neuron_output(inputs, weights2, bias2),
+                neuron_output(inputs, weights3, bias3)]
+
+print(layer_output) # output of neuron1
