cca9decf438a1243831ae3f524aa2a5f710cf0b3
Ali El-Zein <shaclata@gmail.com>
2024-04-24 17:52:25 -0500

    Changed inputs from arr to matrix

--- src/p1.py
+++ src/p1.py
@@ -2,7 +2,11 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1, 2, 3, 2.5] # output from 3 neurons in prev layer
+# batch size is the # of inputs taken at once
+# output from 3 neurons in prev layer
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0],
+          [-1.5, 2.7, 3.3, -0.8]]
 
 weights1 = [0.2, 0.8, -0.5, 1.0] # weight for each input into neuron1
 weights2 = [0.5, -0.91, 0.26, -0.5]
@@ -14,5 +18,7 @@ bias2 = 3
 bias3 = 0.5
 biases = [bias1, bias2, bias3]
 
-layer_output = np.dot(weights, inputs) + biases # calculated output for the 3 neurons in the layer
+# now that inputs are a matrix and not just an array, matrix mult will be performed
+# to make the # of rows in weights (3) match the cols in inputs (4), transpose weights
+layer_output = np.dot(inputs, np.array(weights).T) + biases # calculated output for the 3 neurons in the layer
 print(layer_output)
